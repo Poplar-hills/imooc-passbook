82db2ca80523a39561f5ffacce48f64fd063d979
Max Jiang <maxjiang23326@gmail.com>
2019-12-13 02:20:08 +0800

    first commit

---
+++ src/main/resources/merchants.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `merchants` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL COMMENT '商户名称',
+    `logo_url` VARCHAR(256) NOT NULL COMMENT '商户 logo URL',
+    `business_license_url` VARCHAR(256) NOT NULL COMMENT '商户营业执照 URL',
+    `phone` VARCHAR(64) NOT NULL COMMENT '商户电话',
+    `address` VARCHAR(64) NOT NULL COMMENT '商户地址',
+    `is_audit` BOOLEAN NOT NULL COMMENT '是否通过审核',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARACTER SET=utf8;
