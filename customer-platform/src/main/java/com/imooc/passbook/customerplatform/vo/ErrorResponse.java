98702e7fdb5ad6cb3e9f972bc681829de02575f8
Max Jiang <maxjiang23326@gmail.com>
2020-02-21 20:00:42 +0800

    feat: add error response object

---
+++ customer-platform/src/main/java/com/imooc/passbook/customerplatform/vo/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.imooc.passbook.customerplatform.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 统一的错误信息对象
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse<T> {
+
+    public static final Integer ERROR = -1;
+
+    private Integer code;
+
+    private String message;
+
+    private String requestUrl;  // 在 error 对象中记录请求 url 是很有必要的，便于 debug
+
+    private T data;
+}
