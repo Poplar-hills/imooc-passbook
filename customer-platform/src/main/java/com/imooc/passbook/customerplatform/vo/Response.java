899a20abe1d0f0ebc4d719567fe9b00cbd1d92bb
Max Jiang <maxjiang23326@gmail.com>
2020-02-23 20:50:22 +0800

    feat: add shortcut methods for Response

--- customer-platform/src/main/java/com/imooc/passbook/customerplatform/vo/Response.java
+++ customer-platform/src/main/java/com/imooc/passbook/customerplatform/vo/Response.java
@@ -1,5 +1,6 @@
 package com.imooc.passbook.customerplatform.vo;
 
+import com.imooc.passbook.customerplatform.constants.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +16,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Response<T> {
 
-    public static final Integer ERROR = -1;
+    private Integer code = ErrorCode.SUCCESS.getCode();
 
-    private Integer code;
-
-    private String message;
+    private String message = ErrorCode.SUCCESS.getDesc();
 
     private String requestUrl;  // 在 error 对象中记录请求 url 是很有必要的，便于 debug
 
     private T data;
+
+    public Response(T data) {
+        this.data = data;
+    }
+
+    public static <T> Response<T> success() {  // 成功响应的便捷构造函数
+        return new Response<>();
+    }
+
+    public static <T> Response<T> failure(ErrorCode errorCode) {  // 相当于 merchant-platform Response 类中的 from 方法
+        return Response.<T>builder()
+            .code(errorCode.getCode())
+            .message(errorCode.getDesc())
+            .requestUrl("")
+            .data(null)
+            .build();
+    }
 }
