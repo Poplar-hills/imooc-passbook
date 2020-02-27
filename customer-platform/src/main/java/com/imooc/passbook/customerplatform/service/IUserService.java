57f80e23d7285814777ecd669a9d562f33ead403
Max Jiang <maxjiang23326@gmail.com>
2020-02-27 21:11:14 +0800

    feat: create user in Hbase

--- customer-platform/src/main/java/com/imooc/passbook/customerplatform/service/IUserService.java
+++ customer-platform/src/main/java/com/imooc/passbook/customerplatform/service/IUserService.java
@@ -1,10 +1,9 @@
 package com.imooc.passbook.customerplatform.service;
 
-import com.imooc.passbook.customerplatform.vo.Response;
 import com.imooc.passbook.customerplatform.vo.User;
 
 public interface IUserService {
 
     // 创建用户
-    Response<User> createUser(User user) throws Exception;
+    User createUser(User user) throws Exception;
 }
--- customer-platform/src/main/java/com/imooc/passbook/customerplatform/service/UserServiceImpl.java
+++ customer-platform/src/main/java/com/imooc/passbook/customerplatform/service/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package com.imooc.passbook.customerplatform.service;
 
+import com.imooc.passbook.customerplatform.constants.Constants;
 import com.imooc.passbook.customerplatform.constants.HBaseTable;
-import com.imooc.passbook.customerplatform.utils.RowKeyGenerator;
 import com.imooc.passbook.customerplatform.vo.Response;
 import com.imooc.passbook.customerplatform.vo.User;
 import com.spring4all.spring.boot.starter.hbase.api.HbaseTemplate;
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.utility.RandomString;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 用于在 HBase 中读写 User 的 service
@@ -27,7 +33,7 @@ public class UserServiceImpl implements IUserService {
     }
 
     @Override
-    public Response<User> createUser(User user) throws Exception {
+    public User createUser(User user) throws Exception {
         byte[] FAMILY_B = HBaseTable.UserTable.FAMILY_B.getBytes();
         byte[] NAME = HBaseTable.UserTable.NAME.getBytes();
         byte[] AGE = HBaseTable.UserTable.AGE.getBytes();
@@ -37,10 +43,21 @@ public class UserServiceImpl implements IUserService {
         byte[] PHONE = HBaseTable.UserTable.PHONE.getBytes();
         byte[] ADDRESS = HBaseTable.UserTable.ADDRESS.getBytes();
 
-        Long currUserNum = redisTemplate.opsForV
+        Long currUserCount = redisTemplate.opsForValue()      // 从 Redis 中取出当前系统的用户数（opsForValue 返回所有 String 操作）
+            .increment(Constants.USER_COUNT_REDIS_KEY, 1);  // 在已有用户总数上 +1 就是创建这个用户之后的用户总数
+        Long userId = genUserId(currUserCount);
 
-        Long userId = genUserId(1L);
-        return null;
+        Put put = new Put(Bytes.toBytes(userId));             // 开始组装 HBase put query
+        put.addColumn(FAMILY_B, NAME, Bytes.toBytes(user.getBaseInfo().getName()));
+        put.addColumn(FAMILY_B, AGE, Bytes.toBytes(user.getBaseInfo().getAge()));
+        put.addColumn(FAMILY_B, SEX, Bytes.toBytes(user.getBaseInfo().getSex()));
+        put.addColumn(FAMILY_O, PHONE, Bytes.toBytes(user.getOtherInfo().getPhone()));
+        put.addColumn(FAMILY_O, ADDRESS, Bytes.toBytes(user.getOtherInfo().getAddress()));
+
+        hbaseTemplate.saveOrUpdate(HBaseTable.UserTable.TABLE_NAME, put);  // 将 user 存入 HBase
+
+        user.setId(userId);  // ∵ 传进来的 user 是不带 id 的 ∴ 给其设置 id 后返回
+        return user;
     }
 
     private Long genUserId(Long prefix) {
